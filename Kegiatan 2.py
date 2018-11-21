76698e849ece7aa2da264b052bfb25ef643d97fb
L200183177 <44943945+L200183177@users.noreply.github.com>
2018-11-21 21:15:57 +0700

    Add files via upload

--- Kegiatan 2.py
+++ Kegiatan 2.py
@@ -1,11 +1,9 @@
-## Program Akun
-## Dibuat oleh Alif
-import random
-angka = random.randint(0.1000)
-
-Nama = 'Alif Fauzia Rama Wahyu K'
-TanggalLahir = '27 Mei 2000'
-NamaSingkat = Nama[0] + '. ' + Nama[6] + '. ' + Nama[15:23]
-Username = Nama[0] + TanggalLahir[0] + TanggalLahir[11:15]
-Password = Nama[0:4] + str(angka)
-
+x = input("Masukkan Password : ")
+if x == "Alif":
+    print("Anda Berhasil Login")
+else:
+    x = input("Maaf, Anda Salah Memasukkan Password. Masukkan Password : ")
+    if x == "Alif":
+        print("Anda Berhasil Login")
+    else:
+        print("Anda Telah Mencoba 3 Kali, Akses Anda Ditolak")
