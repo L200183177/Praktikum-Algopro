05ea8c2128e755d5ec6eb89be58ac6c4728f5b0c
L200183177 <44943945+L200183177@users.noreply.github.com>
2018-11-28 23:20:11 +0700

    Add files via upload

--- Kegiatan 1.py
+++ Kegiatan 1.py
@@ -1,11 +1,94 @@
-## Program Identitas Data Diri. Dibuat oleh Alif L200183177
-Nama = 'Alif Fauzia Rama Wahyu K'
-Tempat_Tanggal_Lahir = 'Salatiga 27 Mei 2000'
-Jenis_Kelamin = 'Laki-laki'
-Alamat = 'Aspol Lolong PA II No.2 Padang'
-No_HP = '08197522000'
-Agama = 'Islam'
-Status_Perkawinan = 'Belum Menikah'
-Pekerjaan = 'Pelajar/Mahasiswa'
-Kewarganegaraan = 'Indonesia'
-Golongan_Darah = 'O'
+Data = {"NIM":"L200183177", "Nama":"Alif Fauzia Rama Wahyu K", "Tempat/Tanggal_Lahir":"Salatiga / 27 Mei 2000", "Jenis_Kelamin":"Laki - laki", "Golongan_Darah":"O", "Alamat":"Aspol Lolong PA II No.2 Padang", "Agama":"Islam", "Status_Perkawinan":"Belum Kawin", "Pekerjaan":"Mahasiswa", "Kewarganegaraan":"Indonesia"}
+NIM = Data["NIM"]
+Nama = Data["Nama"]
+TTL = Data["Tempat/Tanggal_Lahir"]
+JK = Data["Jenis_Kelamin"]
+GD = Data["Golongan_Darah"]
+Alamat = Data["Alamat"]
+Agama = Data["Agama"]
+SP = Data["Status_Perkawinan"]
+Pekerjaan = Data["Pekerjaan"]
+Kewarganegaraan = Data["Kewarganegaraan"]
+
+print("Pilihan Yang Tersedia:")
+print("f menampilkan bantuan ini")
+print("z menampilkan NIM")
+print("x menampilkan Nama")
+print("c menampilkan Tempat/Tanggal Lahir")
+print("v menampilkan Jenis Kelamin")
+print("b menampilkan Golongan Darah")
+print("n menampilkan Alamat")
+print("m menampilkan Agama")
+print("a menampilkan Status Perkawinan")
+print("s menampilkan Pekerjaan")
+print("d menampilkan Kewarganegaraan")
+print("g untuk keluar")
+print(" ")
+
+f ='''Pilihan Yang Tersedia:
+f menampilkan bantuan ini
+z menampilkan NIM
+x menampilkan Nama
+c menampilkan Tempat/Tanggal Lahir
+v menampilkan Jenis Kelamin
+b menampilkan Golongan Darah
+n menampilkan Alamat
+m menampilkan Agama
+a menampilkan Status Perkawinan
+s menampilkan Pekerjaan
+d menampilkan Kewarganegaraan
+g untuk keluar'''
+
+g = "Terima Kasih"
+w = input("Masukkan huruf: ")
+while w != "g":
+    if w == "f":
+        print(f)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "z":
+        print(NIM)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "x":
+        print(Nama)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "c":
+        print(TTL)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "v":
+        print(JK)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "b":
+        print(GD)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "n":
+        print(Alamat)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "m":
+        print(Agama)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "a":
+        print(SP)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "s":
+        print(Pekerjaan)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    elif w == "d":
+        print(Kewarganegaraan)
+        print(" ")
+        w = input("Masukkan huruf: ")
+    else:
+        print("perintah tidak dikenal")
+        print(" ")
+        w = input("Masukkan huruf: ")
+print(g)
+
